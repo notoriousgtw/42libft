94c34e67303ce171e5575c3d634539958ebb51b8
Gavin Wood <notoriousgtw@gmail.com>
2018-06-11 19:39:01 -0700

    semicolon

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gwood <gwood@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/19 13:54:20 by gwood             #+#    #+#             */
-/*   Updated: 2018/06/11 19:29:50 by gwood            ###   ########.fr       */
+/*   Updated: 2018/06/11 19:38:45 by gwood            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,7 +102,7 @@ void					ft_lstdelone(t_list **alst,
 							void (*del)(void *, size_t));
 void					ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void					ft_lstadd(t_list **alst, t_list *new);
-void					ft_lstapp(t_list **alst, t_list *new)
+void					ft_lstapp(t_list **alst, t_list *new);
 void					ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list					*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 
